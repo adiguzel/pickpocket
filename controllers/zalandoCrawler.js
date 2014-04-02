cc6597ca6f15ef9a6abe2f37669a18eec9aa31dc
Ugur Adiguzel <uguradgzl88@gmail.com>
2014-04-02 16:37:03 +0200

    got rid of unnecessary self configuration of classes and calls to self

--- controllers/zalandoCrawler.js
+++ controllers/zalandoCrawler.js
@@ -1,19 +1,12 @@
 function ZalandoCrawler(configuration, Crawler, Item) {
-    var self = this;
     var util = require('util');
-
-    this.configure = function(configuration) {
-        this.config = configuration;
-        this.genderTypeShopColorUrlPattern = configuration.urlPatterns.genderTypeShopColor.text;
-    };
     
     this.start = function() {
-        var config = this.config;
-        config.shops.map(function(shop) {
-            config.colors.map(function(color) {
-                config.genders.map(function(gender) {
+        configuration.shops.map(function(shop) {
+            configuration.colors.map(function(color) {
+                configuration.genders.map(function(gender) {
                     gender.clothingTypes.map(function(clothingType) {
-                        findAndQueuePages(shop, color, gender, clothingType, config);
+                        findAndQueuePages(shop, color, gender, clothingType, configuration);
                     })
                 })
             })  
@@ -21,7 +14,7 @@ function ZalandoCrawler(configuration, Crawler, Item) {
     };
 
     var findAndQueuePages = function(shop, color, gender, clothingType, config) {
-        var genderTypeShopColorUrlPattern = self.genderTypeShopColorUrlPattern;
+        var genderTypeShopColorUrlPattern = configuration.urlPatterns.genderTypeShopColor.text;
         var domain = config.domain;
         // Url structure for  shop, gender, clothing  and color filtering
         // http://www.zalando.de/genderClothingFilterString/shopFilterString_colorFilter/
@@ -39,31 +32,26 @@ function ZalandoCrawler(configuration, Crawler, Item) {
             this.domain = domain;
         }
 
-        var paginationExplorer = new ZalandoPaginationExplorer(new CrawlingConfiguration(), Crawler, Item).Crawler();
+        var paginationExplorer = new ZalandoPaginationExplorer(new CrawlingConfiguration(), Crawler, Item);
         paginationExplorer.queue(url);
     };
 
-    this.configure(configuration);
     return this;
 }
 
 module.exports.ZalandoCrawler = ZalandoCrawler
 
 function ZalandoPaginationExplorer(configuration, Crawler, Item) {
-    var self = this;
-    var itemFinder = new ZalandoItemFinder(configuration, Crawler, Item).Crawler();
     var util = require('util');
 
-    this.configure = function(configuration) {
-        this.config = configuration;
-    };
-
-    this.Crawler = function() {
-        return new Crawler({
+    this.queue = function(url) {
+        var crawler = new Crawler({
             "maxConnections": configuration.zalando.crawler.maxConnections,
             "callback": onPaginationSamplePageLoad
         });
-    } 
+
+        crawler.queue(url);
+    }
 
     var onPaginationSamplePageLoad = function(error,result,$) {
         // make sure there are neither any error nor redirects
@@ -75,14 +63,15 @@ function ZalandoPaginationExplorer(configuration, Crawler, Item) {
             if(result != null) {
                 itemDef = result.uri;
             }
-            console.log("error occured or redirect requested for " + itemDef)
+            console.log("Error occured or redirect requested for " + itemDef)
         }
     };  
 
     var queuePages = function(result,$) { 
-        var paginatedUrlPattern = self.config.zalando.urlPatterns.paginatedUrl.text;
-        var paginationSelector = self.config.zalando.selectors.itemResultsPage.pagination.selector;
-        var pageAnchorSelector = self.config.zalando.selectors.itemResultsPage.paginationAnchors.selector;
+        var itemFinder = new ZalandoItemFinder(configuration, Crawler, Item);
+        var paginatedUrlPattern = configuration.zalando.urlPatterns.paginatedUrl.text;
+        var paginationSelector = configuration.zalando.selectors.itemResultsPage.pagination.selector;
+        var pageAnchorSelector = configuration.zalando.selectors.itemResultsPage.paginationAnchors.selector;
         var firstPagination = $(paginationSelector).first();
         var pages = firstPagination.find(pageAnchorSelector);
 
@@ -98,42 +87,38 @@ function ZalandoPaginationExplorer(configuration, Crawler, Item) {
         }
     }; 
 
-    this.configure(configuration);
 }
 
 function ZalandoItemFinder(configuration, Crawler, Item) {
-    var self = this;
-    var itemCrawler = new ZalandoItemCrawler(configuration, Crawler, Item).Crawler();
     var util = require('util');
 
-    this.configure = function(configuration) {
-        this.config = configuration;
-    };
-
-    this.Crawler = function(){
-        return new Crawler({
+    this.queue = function(url) {
+        var crawler = new Crawler({
           "maxConnections": configuration.zalando.crawler.maxConnections,
-          "callback": onItemListPageLoad 
+          "callback": onItemListPageLoad
         });
+
+        crawler.queue(url);
     }
 
     var getSelectors = function() {
-        var config = self.config;
+        var zalando = configuration.zalando;
         var selectors = new Object();
 
-        selectors.itemsSelector = config.zalando.selectors.itemResultsPage.items.selector;
-        selectors.itemLinkSelector = config.zalando.selectors.itemResultsPage.itemAnchors.selector;
-        selectors.itemLinkHrefAttr = config.zalando.selectors.itemResultsPage.itemLinkHrefAttr.selector;
+        selectors.itemsSelector = zalando.selectors.itemResultsPage.items.selector;
+        selectors.itemLinkSelector = zalando.selectors.itemResultsPage.itemAnchors.selector;
+        selectors.itemLinkHrefAttr = zalando.selectors.itemResultsPage.itemLinkHrefAttr.selector;
 
         return selectors;
     }
 
     var findAndQueueItems = function (result,$) {
+        var itemCrawler = new ZalandoItemCrawler(configuration, Crawler, Item);
         var selectors = getSelectors();
         $(selectors.itemsSelector).each(function() {
            $(this).find(selectors.itemLinkSelector).attr(selectors.itemLinkHrefAttr, function( i, href ) {
-            var itemUrlPattern = self.config.zalando.urlPatterns.itemUrl.text;
-            var url = util.format(itemUrlPattern, self.config.domain, href);
+            var itemUrlPattern = configuration.zalando.urlPatterns.itemUrl.text;
+            var url = util.format(itemUrlPattern, configuration.domain, href);
             console.log(url)
             itemCrawler.queue(url);
            });
@@ -145,49 +130,45 @@ function ZalandoItemFinder(configuration, Crawler, Item) {
       if(error == null && result.request._redirectsFollowed == 0)
           findAndQueueItems(result, $) 
       else 
-          console.log("error occured or redirect requested for " + result.uri)
+          console.log("Error occured or redirect requested for " + result.uri)
     };
 
-    this.configure(configuration);
 }
 
 function ZalandoItemCrawler(configuration, Crawler, Item) {
-    var self = this;
 
-    this.configure = function(configuration) {
-        this.config = configuration;
-    };
-
-    this.Crawler = function(){
-        return new Crawler({
+    this.queue = function(url) {
+        var crawler = new Crawler({
             "maxConnections": configuration.zalando.crawler.maxConnections,
             "callback": onItemPageLoad
         });
+
+        crawler.queue(url);
     }
 
     var getSelectors = function() {
-        var config = self.config;
+        var zalando = configuration.zalando;
         var selectors = new Object();
 
-        selectors.brand =  config.zalando.selectors.itemPage.brand.selector;
-        selectors.typeAndColor = config.zalando.selectors.itemPage.typeAndColor.selector;
-        selectors.priceWithCurrency = config.zalando.selectors.itemPage.priceWithCurrency.selector;
-        selectors.oldPriceWithCurrency = config.zalando.selectors.itemPage.oldPriceWithCurrency.selector;
-        selectors.saving = config.zalando.selectors.itemPage.saving.selector;
-        selectors.imageSlide = config.zalando.selectors.itemPage.imageSlide.selector;
-        selectors.image = config.zalando.selectors.itemPage.image.selector;
-        selectors.imageSource = config.zalando.selectors.itemPage.imageSource.selector;
+        selectors.brand =  zalando.selectors.itemPage.brand.selector;
+        selectors.typeAndColor = zalando.selectors.itemPage.typeAndColor.selector;
+        selectors.priceWithCurrency = zalando.selectors.itemPage.priceWithCurrency.selector;
+        selectors.oldPriceWithCurrency = zalando.selectors.itemPage.oldPriceWithCurrency.selector;
+        selectors.saving = zalando.selectors.itemPage.saving.selector;
+        selectors.imageSlide = zalando.selectors.itemPage.imageSlide.selector;
+        selectors.image = zalando.selectors.itemPage.image.selector;
+        selectors.imageSource = zalando.selectors.itemPage.imageSource.selector;
 
         return selectors;
     }
 
     var getSeperators = function() {
-        var config = self.config;
+        var zalando = configuration.zalando;
         var seperators = new Object();
 
-        seperators.imageSources =  config.zalando.seperators.itemPage.imageSources.seperator;
-        seperators.typeAndColor =  config.zalando.seperators.itemPage.typeAndColor.seperator;
-        seperators.priceAndCurrency =  config.zalando.seperators.itemPage.priceAndCurrency.seperator;
+        seperators.imageSources =  zalando.seperators.itemPage.imageSources.seperator;
+        seperators.typeAndColor =  zalando.seperators.itemPage.typeAndColor.seperator;
+        seperators.priceAndCurrency =  zalando.seperators.itemPage.priceAndCurrency.seperator;
 
         return seperators;
     }
@@ -240,10 +221,10 @@ function ZalandoItemCrawler(configuration, Crawler, Item) {
         var saving = $(selectors.saving).text().slice(0, -1);
 
         return new Item({ 
-          gender: self.config.gender.category,
-          type: self.config.clothingType.category,
+          gender: configuration.gender.category,
+          type: configuration.clothingType.category,
           typeText: typeText,
-          color: self.config.color.category,
+          color: configuration.color.category,
           colorText: colorText,
           brand: brand,
           url: result.uri,
@@ -263,7 +244,7 @@ function ZalandoItemCrawler(configuration, Crawler, Item) {
               console.log("Querying failed not crawling : " + result.uri)
            }
            else if(items.length > 0){
-              console.log("There is already an item with the following url : " + result.uri)
+              console.log("An item with the following url already exists : " + result.uri)
            }
            else { //Attempt to save the item only if it is not already in the db
               item.save(function (err, item) {
@@ -279,9 +260,8 @@ function ZalandoItemCrawler(configuration, Crawler, Item) {
         if(error == null && result.request._redirectsFollowed == 0)
             tryCrawlAndSaveItem(result, $)
         else 
-            console.log("error occured or redirect requested for " + result.uri)
+            console.log("Error occured or redirect requested for " + result.uri)
     };
 
-    this.configure(configuration);
 }
 
--- main.js
+++ main.js
@@ -7,7 +7,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
     var Crawler = require("crawler").Crawler;
     var Item = require("./models/item.js").Item(mongoose);
-    var zalandoConfiguration = require("./zalando-test-configuration.json");
+    var zalandoConfiguration = require("./zalando-configuration.json");
     var zalandoCrawler = require("./controllers/zalandoCrawler.js")
   					     .ZalandoCrawler(zalandoConfiguration, Crawler, Item)
   					     .start();
